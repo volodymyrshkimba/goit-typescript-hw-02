374e28696bc050e608be373cf230ddb1f46d27cc
Volodymyr Shkimba <shkimbavova@gmail.com>
2024-11-19 22:38:48 +0200

    do 99% image interface

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,103 @@ import ErrorMessage from "./components/ErrorMessage/ErrorMessage.jsx";
 import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn.jsx";
 import ImageModal from "./components/ImageModal/ImageModal.jsx";
 
+interface ImageAlternativeSlugs {
+  de: string;
+  en: string;
+  es: string;
+  fr: string;
+  it: string;
+  ja: string;
+  ko: string;
+  pt: string;
+}
+
+interface ImageLinks {
+  download: string;
+  download_location: string;
+  html: string;
+  self: string;
+}
+
+interface TopicSubmissions {
+  [key: string]: { status: string; approved_on?: string };
+}
+
+interface ImageUrls {
+  full: string;
+  raw: string;
+  regular: string;
+  small: string;
+  small_s3: string;
+  thumb: string;
+}
+
+interface UserLinks {
+  followers: string;
+  following: string;
+  html: string;
+  likes: string;
+  photos: string;
+  portfolio: string;
+  self: string;
+}
+
+interface UserProfileImage {
+  large: string;
+  medium: string;
+  small: string;
+}
+
+interface User {
+  accepted_tos: boolean;
+  bio: string | null;
+  first_name: string;
+  for_hire: boolean;
+  id: string;
+  instagram_username: string | null;
+  last_name: string | null;
+  links: UserLinks;
+  location: string | null;
+  name: string;
+  portfolio_url: string | null;
+  profile_image: UserProfileImage;
+  social: {}; // ====================================================================================================
+  total_collections: number;
+  total_illustrations: number;
+  total_likes: number;
+  total_photos: number;
+  total_promoted_illustrations: number;
+  total_promoted_photos: number;
+  twitter_username: string | null;
+  updated_at: string;
+  username: string;
+}
+
+interface Image {
+  alt_desctiption: string;
+  alternative_slugs: ImageAlternativeSlugs;
+  asset_type: string;
+  blur_hash: string;
+  breadcrumbs: unknown[];
+  color: string;
+  created_at: string;
+  current_user_collections: unknown[];
+  description: string | null;
+  height: number;
+  id: string;
+  liked_by_user: boolean;
+  likes: number;
+  links: ImageLinks;
+  promoted_at: string | null;
+  slug: string;
+  sponsorship: null;
+  topic_submissions: TopicSubmissions;
+  updated_at: string;
+  urls: ImageUrls;
+  user: User;
+  width: number;
+}
+
 function App() {
   const [images, setImages] = useState([]);
   const [keyWord, setKeyword] = useState<string>("");
@@ -41,6 +138,18 @@ function App() {
   const onLoadMore = () => {
     setPage(page + 1);
   };
+
+  useEffect(() => {
+    images.forEach((image) => {
+      const userKeys = Object.keys(image.user.profile_image);
+
+      userKeys.forEach((userKey) => {
+        if (image.user.profile_image[userKey] === null) {
+          console.log(userKey, "=", image.user.profile_image[userKey]);
+        }
+      });
+    });
+  }, [images]);
 
   useEffect(() => {
     if (!keyWord) return;
